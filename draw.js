b6f60a146506e481293f7baf03f15ee2b68a2ec7
Doojin Kang <doojin.kang@gmail.com>
2022-04-25 00:08:29 +0900

    apply eventTouchToMouse to draw

--- draw.js
+++ draw.js
@@ -72,28 +72,37 @@
     mousePos = getMousePos(canvas, e);
   }, false);
 
-  // Set up touch events for mobile, etc
-  canvas.addEventListener("touchstart", function (e) {
-    mousePos = getTouchPos(canvas, e);
-    var touch = e.touches[0];
-    var mouseEvent = new MouseEvent("mousedown", {
-      clientX: touch.clientX,
-      clientY: touch.clientY
-    });
-    canvas.dispatchEvent(mouseEvent);
-  }, false);
-  canvas.addEventListener("touchend", function (e) {
-    var mouseEvent = new MouseEvent("mouseup", {});
-    canvas.dispatchEvent(mouseEvent);
-  }, false);
-  canvas.addEventListener("touchmove", function (e) {
-    var touch = e.touches[0];
-    var mouseEvent = new MouseEvent("mousemove", {
-      clientX: touch.clientX,
-      clientY: touch.clientY
-    });
-    canvas.dispatchEvent(mouseEvent);
-  }, false);
+  function eventTouchToMouse(_div) {
+    function getTouchPos(div, touchEvent) {
+      var rect = div.getBoundingClientRect();
+      return {
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top
+      };
+    }
+    _div.addEventListener("touchstart", function (e) {
+      mousePos = getTouchPos(_div, e);
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+      });
+      _div.dispatchEvent(mouseEvent);
+    }, false);
+    _div.addEventListener("touchend", function (e) {
+      var mouseEvent = new MouseEvent("mouseup", {});
+      _div.dispatchEvent(mouseEvent);
+    }, false);
+    _div.addEventListener("touchmove", function (e) {
+      var touch = e.touches[0];
+      var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+      });
+      _div.dispatchEvent(mouseEvent);
+    }, false);
+  }
+  eventTouchToMouse(canvas);
 
   // Prevent scrolling when touching the canvas
   document.body.addEventListener("touchstart", function (e) {
@@ -123,13 +132,6 @@
   }
 
   // Get the position of a touch relative to the canvas
-  function getTouchPos(canvasDom, touchEvent) {
-    var rect = canvasDom.getBoundingClientRect();
-    return {
-      x: touchEvent.touches[0].clientX - rect.left,
-      y: touchEvent.touches[0].clientY - rect.top
-    };
-  }
 
   // Draw to the canvas
   function renderCanvas() {
